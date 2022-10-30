da197e156f744e92918886d521ea79fa4f79122b
Gibooon <61015424+Gibooon@users.noreply.github.com>
2022-10-30 12:41:31 +0100

    aktualizacja app.module.ts (jakies bledy do sprawdzenia)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,13 @@ import { AppComponent } from './app.component';
 
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { ProjectDetailComponent } from './projects/project-detail/project-detail.component';
+import { ProjectListComponent } from './projects/project-list/project-list.component';
+import { ProjectItemComponent } from '.projects/project-list/project-item/project-item.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, NavbarComponent, ProjectsComponent],
+  declarations: [AppComponent, NavbarComponent, ProjectsComponent ProjectDetailComponent, ProjectListComponent, ProjectItemComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
