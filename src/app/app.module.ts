a087e778572e6238549170e874749486cb9e0393
Gibooon <61015424+Gibooon@users.noreply.github.com>
2022-12-27 22:05:06 +0100

    3 files modified, 12 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 import { NavbarComponent } from '../components/navbar/navbar.component';
-import { ProjectsComponent } from '../components/projects/projects.component';
-import { ProjectDetailComponent } from '../components/projects/project-detail/project-detail.component';
-import { ProjectListComponent } from '../components/projects/project-list/project-list.component';
-import { ProjectItemComponent } from '../components/projects/project-list/project-item/project-item.component';
 import { AddProjectComponent } from '../components/add-project/add-project.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    ProjectsComponent,
-    ProjectDetailComponent,
-    ProjectListComponent,
-    ProjectItemComponent,
-    AddProjectComponent,
-  ],
+  declarations: [AppComponent, NavbarComponent, AddProjectComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
