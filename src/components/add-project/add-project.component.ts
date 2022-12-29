980bc908657df9adf16c5ab7f47fae89d3561902
Gibooon <61015424+Gibooon@users.noreply.github.com>
2022-12-29 23:02:40 +0100

    1 file modified

--- src/components/add-project/add-project.component.ts
+++ src/components/add-project/add-project.component.ts
@@ -11,17 +11,21 @@ export class AddProjectComponent implements OnInit {
   ngOnInit() {}
 
   onAddProject() {
+    //funkcja dodająca projekt
     const cont = document.getElementById('content');
-    const Name = document.getElementById('name') as HTMLInputElement;
-    const Color = document.getElementById('color') as HTMLInputElement;
-    const Data = document.getElementById('date') as HTMLInputElement;
-    const Desc = document.getElementById('description') as HTMLInputElement;
-    cont.innerHTML += `<div  class="projects" style='height:99.5%; width:25%; background-color:${Color.value}; border:2px black solid;'>
-      Nazwa projektu: ${Name.value}
-      <hr>
-      Deadline: ${Data.value}
-      <hr>
-      Opis: ${Desc.value}
+    const name = document.getElementById('name') as HTMLInputElement;
+    const color = document.getElementById('color') as HTMLInputElement;
+    const data = document.getElementById('date') as HTMLInputElement;
+    const desc = document.getElementById('description') as HTMLInputElement;
+    const stat = 'In progress'; //default
+    cont.innerHTML += `<div  class="projects" style='height:99.5%; width:25%; background-color:${color.value}; border:2px black solid;'>
+      Nazwa projektu: ${name.value}
+      <hr style='font-weight:bold;'>
+      Deadline: ${data.value}
+      <hr style='font-weight:bold;'>
+      Status: <stat>${stat}</stat> <select id="status${name.value}" (change)="onChangeStatus()"><option value="0">In progress</option><option value="1">Finished</option><option value="2">Abandoned</option></select>
+      <hr style='font-weight:bold;'>
+      Opis: ${desc.value}
       </div>`;
   }
 }
