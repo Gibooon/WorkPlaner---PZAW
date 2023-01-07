bbc769b2d9c980865727fa843a1c45095ae565be
Gibooon <61015424+Gibooon@users.noreply.github.com>
2023-01-07 12:02:17 +0100

    Finał version

--- src/components/add-project/add-project.component.ts
+++ src/components/add-project/add-project.component.ts
@@ -17,13 +17,10 @@ export class AddProjectComponent implements OnInit {
     const color = document.getElementById('color') as HTMLInputElement;
     const data = document.getElementById('date') as HTMLInputElement;
     const desc = document.getElementById('description') as HTMLInputElement;
-    const stat = 'In progress'; //default
     cont.innerHTML += `<div  class="projects" style='height:99.5%; width:25%; background-color:${color.value}; border:2px black solid;'>
       Nazwa projektu: ${name.value}
       <hr style='font-weight:bold;'>
       Deadline: ${data.value}
-      <hr style='font-weight:bold;'>
-      Status: <stat>${stat}</stat> <select id="status${name.value}" (change)="onChangeStatus()"><option value="0">In progress</option><option value="1">Finished</option><option value="2">Abandoned</option></select>
       <hr style='font-weight:bold;'>
       Opis: ${desc.value}
       </div>`;
